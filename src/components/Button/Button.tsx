b240f15d0b783a70ea1bd8494bf796ef5e39ddbb
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-10-15 19:04:14 -0300

    clase 8 de octubre

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,41 +1,17 @@
 import {ReactNode} from "react";
 
 interface ButtonProps {
-    size?: 'small' | 'medium' | 'large';
-    variant?: 'fulfilled' | 'outlined' | 'ghost';
-    onClick: () => void;
-    icon?: string;
-    disabled?: boolean;
-    children: ReactNode; // Use children instead of text for more flexibility
-
+    size?: 'small' | 'medium' | 'large'
+    variant?: 'fulfilled' | 'outlined' | 'ghost'
+    onClick: () => void
+    icon?: string
+    disabled?: boolean
+    //text: string
+    callback: () => void
 }
 
-//text: string
 //use children: ReactNode instead?
 
-export default function Button({
-                                   size = 'medium',
-                                   variant = 'fulfilled',
-                                   onClick,
-                                   icon,
-                                   disabled = false,
-                                   children,
-                                   callback,
-                               }: ButtonProps) {
-    const sizeClass = size === 'small' ? 'btn-small' : size === 'large' ? 'btn-large' : 'btn-medium';
-    const variantClass = variant === 'outlined' ? 'btn-outlined' : variant === 'ghost' ? 'btn-ghost' : 'btn-fulfilled';
-
-    return (
-        <button
-            className={`btn ${sizeClass} ${variantClass}`}
-            onClick={() => {
-                onClick();
-                if (callback) callback();
-            }}
-            disabled={disabled}
-        >
-            {icon && <img src={icon} alt="button icon" className="icon"/>} {}
-            {children} {}
-        </button>
-    );
+export default function Button({size, variant, text, callbac}) {
+    return (<button className={} onClick={onClick}>{}</button>)
 }
